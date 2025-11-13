96a974a3ca44fe1a06dde21bc4d08c584484949c
Valk <kravchenkoval@mail.ru>
2025-11-13 07:45:11 +0300

    Исправления

--- src/main/java/com/kanban/manager/TaskManager.java
+++ src/main/java/com/kanban/manager/TaskManager.java
@@ -1,5 +1,6 @@
 package com.kanban.manager;
 
+import com.kanban.Main;
 import com.kanban.model.*;
 
 import java.util.*;
@@ -168,41 +169,6 @@ public class TaskManager {
             epic.applyCalculatedStatus(TaskStatus.DONE);
         } else {
             epic.applyCalculatedStatus(TaskStatus.IN_PROGRESS);
-        }
-    }
-
-    public static void main(String[] args) {
-        TaskManager manager = new TaskManager();
-        System.out.println("===== Сценарий 1: Базовое создание задач =====");
-        scenario1_createTasks(manager);
-    }
-
-    private static void scenario1_createTasks(TaskManager manager) {
-        Task task1 = new Task("Переезд", "Собрать вещи", TaskStatus.NEW);
-        Task task2 = new Task("Обучение Java", "Завершить модуль по ООП", TaskStatus.IN_PROGRESS);
-        manager.addTask(task1);
-        manager.addTask(task2);
-        Epic epic1 = new Epic("Запуск проекта Kanban", "Сделать первую версию трекера");
-        Epic epic2 = new Epic("Подготовка к отпуску", "Все спланировать");
-        Subtask s1 = new Subtask("Сделать модель задач", "Task/Epic/Subtask/Status", TaskStatus.NEW, epic1.getId());
-        Subtask s2 = new Subtask("Реализовать менеджер задач", "CRUD + правила эпика", TaskStatus.NEW, epic1.getId());
-        Subtask s3 = new Subtask("Собрать вещи", "Сумка, документы", TaskStatus.NEW, epic2.getId());
-        manager.addSubtask(s1);
-        manager.addSubtask(s2);
-        manager.addSubtask(s3);
-        printAll(manager);
-        System.out.println("\nПроверка get*ById:");
-        System.out.println("Task по id=" + task1.getId() + ": " + manager.getTaskById(task1.getId()).getName());
-    }
-
-    private static void printAll(TaskManager manager) {
-        System.out.println("\n--- Все обычные задачи ---");
-        if (manager.getAllTasks().isEmpty()) {
-            System.out.println("(пусто)");
-        } else {
-            manager.getAllTasks().forEach(task ->
-                    System.out.println("Task: " + task.getId() + " | " + task.getName() + " | " + task.getStatus())
-            );
         }
     }
 }
