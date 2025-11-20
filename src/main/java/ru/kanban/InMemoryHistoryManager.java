8e5e1f6271737e030e11298807e1c695c46bedca
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-11-20 20:14:08 +0300

    24.1 Kanban part 3

--- src/main/java/ru/kanban/InMemoryHistoryManager.java
+++ src/main/java/ru/kanban/InMemoryHistoryManager.java
@@ -4,12 +4,13 @@ import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private static final int MAX_HISTORY_SIZE = 10;
+    private static final int FIRST_ELEMENT_INDEX = 0;
     private final List<BaseTask> history = new ArrayList<>();
 
     @Override
     public void addToHistory(BaseTask task) {
         if (history.size() == MAX_HISTORY_SIZE) {
-            history.remove(0);
+            history.remove(FIRST_ELEMENT_INDEX);
         }
         history.add(task);
     }
